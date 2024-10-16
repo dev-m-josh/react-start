76d695587725928978c0c81c964456b86d74dc87
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-10-16 14:56:35 +0300

    show a single single

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,13 +1,15 @@
 
 import todolist from './todolist';
+import Todo from './Todo';
 
 import React from 'react'
 
 export default function Todos() {
+    
   return (
     <div>
         {todolist.map(todo=>{
-            return todo.task
+            return <Todo key={todo.id} todo={todo} />
         })}
     </div>
   )
